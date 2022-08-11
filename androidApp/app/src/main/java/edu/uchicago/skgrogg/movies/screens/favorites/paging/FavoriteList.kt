a5c976a1cc29520b1640a1fa0dc408e89cacbc36
Sean K. Grogg <Sean.K.Grogg@gmail.com>
2022-08-11 11:02:06 -0400

    Got the favorites list to load, updates when user adds a new movie. However, cannot scrolling yet.

--- androidApp/app/src/main/java/edu/uchicago/skgrogg/movies/screens/favorites/paging/FavoriteList.kt
+++ androidApp/app/src/main/java/edu/uchicago/skgrogg/movies/screens/favorites/paging/FavoriteList.kt
@@ -4,13 +4,20 @@ import android.util.Log
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.paging.LoadState
 import androidx.paging.PagingSource
@@ -28,13 +35,22 @@ import edu.uchicago.skgrogg.movies.viewmodels.MovieViewModel
 fun FavoriteList(favoritesViewModel: FavoriteViewModel, navController: NavController) {
         Log.d("Favorites Loading", "Favorite List")
 
-        val response = favoritesViewModel.favoritesRepository.getFavorites()
-
-        if (response.isExecuted) {
-            favoritesViewModel.getFavorites();
-            val favorites = favoritesViewModel.getList().value
-            favorites?.forEach{ favorite ->
-               FavoriteRow(favorite)
+        favoritesViewModel.getFavorites()
+        if (favoritesViewModel.mFavorites.value?.size == 0){
+            Log.d("Favorites Loading", "Empty List")
+            Text(text = "No Favorites Yet For This User!",
+                modifier = Modifier
+                    .fillMaxWidth(),
+                style = TextStyle(color = Color.Black, fontWeight = FontWeight.Bold),
+                textAlign = TextAlign.Center,
+                fontSize = 20.sp)
+        }else{
+            Log.d("Favorites Loading", "Trying the Row Loop")
+            favoritesViewModel.mFavorites.value?.forEach{ favorite ->
+                FavoriteRow(favorite)
             }
         }
+        Log.d("Favorites Loading", "Response is")
+
+
 }
