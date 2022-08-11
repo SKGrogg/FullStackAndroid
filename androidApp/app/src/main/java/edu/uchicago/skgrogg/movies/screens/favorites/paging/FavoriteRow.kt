3fe08f52b127481a88a241834f715071666ea14f
Sean K. Grogg <Sean.K.Grogg@gmail.com>
2022-08-11 16:21:26 -0400

    Change the layout of delete button on favoriteRow.

--- androidApp/app/src/main/java/edu/uchicago/skgrogg/movies/screens/favorites/paging/FavoriteRow.kt
+++ androidApp/app/src/main/java/edu/uchicago/skgrogg/movies/screens/favorites/paging/FavoriteRow.kt
@@ -1,14 +1,15 @@
 package edu.uchicago.skgrogg.movies.screens.favorites.paging
 
 
-import android.util.Log
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.Button
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
@@ -16,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -23,10 +25,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.accompanist.coil.rememberCoilPainter
-import edu.uchicago.skgrogg.movies.common.Constants
 import edu.uchicago.skgrogg.movies.models.Favorite
 
-import edu.uchicago.skgrogg.movies.models.Result
 import edu.uchicago.skgrogg.movies.viewmodels.FavoriteViewModel
 
 
@@ -35,7 +35,6 @@ import edu.uchicago.skgrogg.movies.viewmodels.FavoriteViewModel
 fun FavoriteRow(favorite: Favorite,
     favoriteViewModel: FavoriteViewModel)
 {
-    Log.d("Favorites Loading", "Made it to Favorite Row")
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -64,25 +63,38 @@ fun FavoriteRow(favorite: Favorite,
 
 
             Column() {
+                Row() {
+                    Text(
+                        //sometimes, the authors are null; for example when it is a United Nations report
+                        text = favorite.title ?: "None",
+                        style = TextStyle(fontWeight = FontWeight.Bold),
+                        textAlign = TextAlign.Center,
+                        fontSize = 18.sp
+                    )
+                    Icon(imageVector = Icons.Default.Delete,
+                        contentDescription = "Delete",
+                        modifier = Modifier
+                            .padding(15.dp, 0.dp, 0.dp, 0.dp)
+                            .clickable { favoriteViewModel.onDelete(favorite) },
+                        tint = Color.Red
+                    )
+                }
 
-                Text(
-                    //sometimes, the authors are null; for example when it is a United Nations report
-                    text = favorite.title ?: "None",
-                    style = TextStyle(fontWeight = FontWeight.Bold),
-                    textAlign = TextAlign.Start,
-                    fontSize = 18.sp
-                )
                 Text(text = favorite.overview ?: "None")
-                Icon(imageVector = Icons.Default.Delete,
-                    contentDescription = "Delete Favorite",
-                    modifier = Modifier
-                        .clickable {
-                            favoriteViewModel.onDelete(favorite)
-                        }
-                        .padding(20.dp, 0.dp, 0.dp, 0.dp))
 
 
             }
+
+            Button(onClick = { favoriteViewModel.onDelete(favorite) }) {
+                Icon(imageVector = Icons.Default.Delete,
+                    contentDescription = "Delete",
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .padding(20.dp, 0.dp, 0.dp, 0.dp))
+            }
+
+
+
         }
     }
 }
